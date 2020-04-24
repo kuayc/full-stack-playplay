322e1b83f8297a7650fa225ea51637c31dffdb28
Chin Kuay <kuaychin@gmail.com>
2020-04-24 10:57:44 -0500

    middleware

--- react-hooks-complete-fullstack/Server/main/app.js
+++ react-hooks-complete-fullstack/Server/main/app.js
@@ -1,26 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const app = express();
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const indexRouter = require("./routes");
 
-var indexRouter = require('./routes')
-
-
-
-
-
-var app = express();
-
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.use('/', indexRouter)
-
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", indexRouter);
 
 module.exports = app;
